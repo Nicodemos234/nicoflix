6524f8fbeef9d640fda5667b4dbbbe612491140d
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-28 22:57:38 -0300

    Adding Body and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div style={background: "#141414"}>
+    <div style={{ background: "#141414" }} >
       <Menu />
+
+      <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é o Front-End?"} />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+      />
+      <Footer />
     </div>
   );
 }
