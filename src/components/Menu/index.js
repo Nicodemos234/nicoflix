e97ad67b112da651d51ea63b6b37dc20e48ffae7
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-30 21:07:56 -0300

    Aula 02

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
 import './Menu.css';
 import Button from '../Button';
 
+
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="NicoFlix Logo" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
 
